227e1b6802b0dfa6df376d457efc4fdf4e965144
kazuma1989 <kazuma.ebina@quipper.com>
2023-09-29 17:08:35 +0900

    apply plugin-legacy and build failed

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vike/plugin'
-import { UserConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import ssr from "vike/plugin";
+import { UserConfig } from "vite";
 
 const config: UserConfig = {
-  plugins: [react(), ssr()]
-}
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+    react(),
+    ssr(),
+  ],
+};
 
-export default config
+export default config;
